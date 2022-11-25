c744b9d1b1548d2303a428e0a5dd8e723ebab97d
c00p75_ <georgecoopmsapenda@gmail.com>
2022-11-26 01:59:36 +0200

    Remove duplicate js code

--- JS/Menu btn.js
+++ JS/Menu btn.js
@@ -50,23 +50,3 @@ sponsors.addEventListener('click', (e) => {
     slide();
   }
 });
-
-// See more | See less
-const seeMore = document.querySelectorAll('.see-more');
-const seeLess = document.querySelectorAll('.see-less');
-const hiddenSpeakers = document.querySelectorAll('.speakers .d-none');
-
-function toggleHide(index, Nodelist) {
-  seeMore[index].classList.toggle('d-none');
-  for (let i = 0; i < Nodelist.length; i += 1) {
-    Nodelist[i].classList.toggle('d-none');
-  }
-}
-
-seeMore[0].addEventListener('click', () => {
-  toggleHide(0, hiddenSpeakers);
-});
-
-seeLess[0].addEventListener('click', () => {
-  toggleHide(0, hiddenSpeakers);
-});
