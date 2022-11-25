6effeb23094f918042f22202a7d342d68b170f36
c00p75_ <georgecoopmsapenda@gmail.com>
2022-11-26 00:44:07 +0200

    fix lint errors

--- JS/Menu btn.js
+++ JS/Menu btn.js
@@ -1,4 +1,4 @@
-//Menu btn functionality
+// Menu btn functionality
 const menuButton = document.querySelector('.menu-btn');
 const navigation = document.querySelector('nav');
 menuButton.addEventListener('click', () => {
@@ -6,71 +6,67 @@ menuButton.addEventListener('click', () => {
   navigation.classList.toggle('bg-dark');
   navigation.classList.toggle('mobile-menu-bg');
   navigation.classList.toggle('position-fixed');
-  document.querySelector('body').classList.toggle('position-fixed')
-  document.querySelector('.logo-img').classList.toggle('d-none')
-  document.querySelector('.nav-bar .logo').classList.toggle('d-none')
-  menuButton.innerHTML = `<img src="Images/close_mobile_menu.png" alt="menu">`
+  document.querySelector('body').classList.toggle('position-fixed');
+  document.querySelector('.logo-img').classList.toggle('d-none');
+  document.querySelector('.nav-bar .logo').classList.toggle('d-none');
+  menuButton.innerHTML = '<img src="Images/close_mobile_menu.png" alt="menu">'
   if (!navigation.classList.contains('bg-dark')) {
-    menuButton.innerHTML = `<img src="Images/mobile_menu.png" alt="menu">`
+    menuButton.innerHTML = '<img src="Images/mobile_menu.png" alt="menu">';
   }
   setTimeout(() => {
     menuButton.classList.remove('diasablePointer');
   }, 500);
-})
-
+});
 
 // Sponsors slide
 const sponsors = document.querySelector('.sponsors');
 const sponsors1 = document.querySelectorAll('.sponsor1');
 const sponsors2 = document.querySelectorAll('.sponsor2');
 const sponsors3 = document.querySelectorAll('.sponsor3');
-const previous = document.querySelectorAll('.prev');
-const next = document.querySelectorAll('.next');
 let slideIndex = 0;
 const sets = [sponsors1, sponsors2, sponsors3];
 
-function hideSponsors (set) {
-  for (let i=0; i < set.length; i++) {
+function hideSponsors(set) {
+  for (let i = 0; i < set.length; i += 1) {
     set[i].classList.toggle('d-md-none')
   }
 }
 
 function slide () {
-  if(slideIndex < 0) {slideIndex = 2};
-  if(slideIndex > 2) {slideIndex = 0};
-  hideSponsors(sets[slideIndex])
+  if (slideIndex < 0) { slideIndex = 2 }
+  if (slideIndex > 2) { slideIndex = 0 }
+  hideSponsors(sets[slideIndex]);
 }
 
 sponsors.addEventListener('click', (e) => {
-  if (e.target.classList.contains('prev')){
-    slide ();
-    slideIndex = slideIndex-1;
-    slide ();
+  if (e.target.classList.contains('prev')) {
+    slide();
+    slideIndex -= 1;
+    slide();
   }
-  if (e.target.classList.contains('next')){
-    slide ();
-    slideIndex = slideIndex + 1;
-    slide ();
+  if (e.target.classList.contains('next')) {
+    slide();
+    slideIndex += 1;
+    slide();
   }
-})
+});
 
 // See more | See less
 const seeMore = document.querySelectorAll('.see-more');
 const seeLess = document.querySelectorAll('.see-less');
-const hiddenSpeakers =  document.querySelectorAll('.speakers .d-none');
+const hiddenSpeakers = document.querySelectorAll('.speakers .d-none');
 
 function toggleHide(index, Nodelist) {
   seeMore[index].classList.toggle('d-none');
-  for (let i=0; i < Nodelist.length; i++) {    
+  for (let i = 0; i < Nodelist.length; i += 1) {
     Nodelist[i].classList.toggle('d-none');
   }
 }
 
 seeMore[0].addEventListener('click', () => {
   toggleHide(0, hiddenSpeakers);
-})
+});
 
 seeLess[0].addEventListener('click', () => {
   toggleHide(0, hiddenSpeakers);
-})
-
+});
--- JS/Script.js
+++ JS/Script.js
@@ -3,57 +3,57 @@ const speakers = [
   {
     name: 'Barack Obama',
     link: 'https://en.wikipedia.org/wiki/Barack_Obama',
-    image:'Images/speaker_01.png',
+    image: 'Images/speaker_01.png',
     title: 'American Politician in the Democratic Party',
-    description:'An American politician who served as the 44th president of the United States from 2009 to 2017.'
+    description:'An American politician who served as the 44th president of the United States from 2009 to 2017.',
   },
 
   {
     name: 'Mizinga Melu',
     link: 'https://en.wikipedia.org/wiki/Mizinga_Melu',
-    image:'Images/speaker_07.png',
+    image: 'Images/speaker_07.png',
     title: 'Zambian businesswoman, accountant, and bank executive',
-    description:'industry expert who has been at the helm of top banking organizations having worked in six (6) African countries.'
+    description:'industry expert who has been at the helm of top banking organizations having worked in six (6) African countries.',
   },
 
   {
     name: 'Lawrence Page',
     link: 'https://en.wikipedia.org/wiki/Larry_Page',
-    image:'Images/speaker_02.png',
+    image: 'Images/speaker_02.png',
     title: 'Co-founder of Google',
-    description:'American business magnate, computer scientist and internet entrepreneur.'
+    description:'American business magnate, computer scientist and internet entrepreneur.',
   },
 
   {
     name: 'Oprah Winfrey',
     link: 'https://en.wikipedia.org/wiki/Oprah_Winfrey',
-    image:'Images/speaker_04.png',
+    image: 'Images/speaker_04.png',
     title: 'American television personality',
-    description:'American talk show host, television producer, actress, author, and philanthropist.'
+    description:'American talk show host, television producer, actress, author, and philanthropist.',
   },
 
   {
     name: 'Mark Nkowani',
     link: '#',
-    image:'Images/speaker_05.png',
+    image: 'Images/speaker_05.png',
     title: 'Investor and CEO',
-    description:'Zambian billionaire and founder of 12 of the top 40 most profitable businesses in Africa'
+    description:'Zambian billionaire and founder of 12 of the top 40 most profitable businesses in Africa',
   },
 
   {
     name: 'Joe Rogan',
     link: 'https://www.joerogan.com/',
-    image:'Images/speaker_09.png',
+    image: 'Images/speaker_09.png',
     title: 'American UFC color commentator, podcaster, and more',
-    description:'Hosts The Joe Rogan Experience, a podcast in which he discusses current events, politics, philosophy, and science'
+    description:'Hosts The Joe Rogan Experience, a podcast in which he discusses current events, politics, philosophy, and science',
   },
 
   {
     name: 'Isaac Phiri',
     link: '#',
-    image:'Images/speaker_03.png',
+    image: 'Images/speaker_03.png',
     title: 'Most recognized real estate lawyer in Zambia',
-    description:'Focuses on the restructuring and liquidation of companies in a wide array of industries.'
+    description:'Focuses on the restructuring and liquidation of companies in a wide array of industries.',
   },
 
   {
@@ -61,35 +61,35 @@ const speakers = [
     link: '#',
     image:'Images/speaker_10.png',
     title: 'Entrepreneur, educator, and business woman',
-    description:"Multi-industry business owner and major share holder of the world's most popular beauty brand"
+    description:"Multi-industry business owner and major share holder of the world's most popular beauty brand",
   },
 
   {
     name: "Chizaso M'sapenda",
     link: '#',
-    image:'Images/speaker_06.png',
+    image: 'Images/speaker_06.png',
     title: 'Founder of one the leading law firms in Zambia',
-    description:'Renowned for work across disciplines, including both transactional and disputes work'
+    description:'Renowned for work across disciplines, including both transactional and disputes work',
   },
 
   {
     name: 'Mia Mottley',
     link: 'https://en.wikipedia.org/wiki/Mia_Mottley',
-    image:'Images/speaker_08.png',
+    image: 'Images/speaker_08.png',
     title: 'Barbadian politician and attorney',
-    description:'Barbadian politician and attorney who has served as prime minister of Barbados since 2018'
-  }
+    description:'Barbadian politician and attorney who has served as prime minister of Barbados since 2018',
+  },
 ];
 
-function addSpeaker (speaker, display=false) {
+function addSpeaker(speaker, display = false) {
   const speakerHTML = document.createElement('div');
   speakerHTML.classList.add('row');
   speakerHTML.classList.add('speaker-card');
   speakerHTML.classList.add('col-md-6');
-  speakerHTML.classList.add('py-4')
-  speakerHTML.classList.add('w3-animate-top')
-  
-  if (display==false){speakerHTML.classList.add('d-none');}
+  speakerHTML.classList.add('py-4');
+  speakerHTML.classList.add('w3-animate-top');
+
+  if (display===false) { speakerHTML.classList.add('d-none'); }
   speakerHTML.innerHTML = `  
     <div class="col-6">
       <img src="${speaker.image}" alt="${speaker.name}" class="img-fluid">
@@ -103,14 +103,34 @@ function addSpeaker (speaker, display=false) {
           <p>${speaker.description}</p>
         </div>
     </div>
-  `
-  document.querySelector('#speaker-container').appendChild(speakerHTML)
+  `;
+  document.querySelector('#speaker-container').appendChild(speakerHTML);
 }
 
 for (let i = 0; i < speakers.length; i++) {
   if (i < 4) {
     addSpeaker(speakers[i], true);
   } else {
-  addSpeaker(speakers[i]);
+    addSpeaker(speakers[i]);
   }
 }
+
+// See more & See less
+const seeMore = document.querySelectorAll('.see-more');
+const seeLess = document.querySelectorAll('.see-less');
+const hiddenSpeakers = document.querySelectorAll('.speakers .d-none');
+
+function toggleHide(index, Nodelist) {
+  seeMore[index].classList.toggle('d-none');
+  for (let i = 0; i < Nodelist.length; i++) {
+    Nodelist[i].classList.toggle('d-none');
+  }
+}
+
+seeMore[0].addEventListener('click', () => {
+  toggleHide(0, hiddenSpeakers);
+});
+
+seeLess[0].addEventListener('click', () => {
+  toggleHide(0, hiddenSpeakers);
+});
